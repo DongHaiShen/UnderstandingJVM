7537a42611af5274d374bd299aa68dc3b1a7336a
sdh <215936795@qq.com>
2019-03-20 21:24:46 +0800

    chapter06

---
+++ 第6章-类文件结构/TestClass.java
@@ -0,0 +1,17 @@
+/**
+ * @Author sdh
+ * @Date Created in 2019/3/18 16:41
+ * @description
+ */
+package org.fenixsoft.clazz;
+
+public class TestClass {
+
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
+
+
